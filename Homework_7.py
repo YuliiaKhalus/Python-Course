377d39afbacc7050a8cab248e1f8b4fd273daf90
admin <khalusyuliiajri@gmail.com>
2020-07-02 15:35:10 +0300

    Updated Tasks 1 and 2

--- Homework_7.py
+++ Homework_7.py
@@ -3,6 +3,7 @@ class RomanNumber(int):
 
     @staticmethod
     def int_to_roman(number):
+        number = (int(number))
         ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
         nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
         result = []
@@ -12,13 +13,18 @@ class RomanNumber(int):
             number -= ints[i] * count
         return ''.join(result)
 
-    def __str__(self):
+    def __new__(cls, *args, **kwargs):
         try:
-            if 0 < self < 4000:
-                return self.int_to_roman(self)
-            raise ValueError
+            for arg in args:
+                if 0 < arg < 4000:
+                    instance = super().__new__(cls, *args, **kwargs)
+                    return instance
+                raise ValueError
         except ValueError:
             return 'The value of Roman numbers should be within the range 1-3999.'
+
+    def __str__(self):
+        return self.int_to_roman(self)
 
     def __add__(self, other):
         return RomanNumber(super().__add__(other))
@@ -36,6 +42,7 @@ class RomanNumber(int):
 V = RomanNumber(5)
 X = RomanNumber(10)
 IX = RomanNumber(9)
+
 
 a = X + IX
 b = IX - V
@@ -61,11 +68,16 @@ print(b)
 print(a + b)
 print(V + 500000)
 
+
 # Task 2
-class RangeFunction():
+class RangeFunction:
+
     def __init__(self, *limits):
+        self.step = 1
         if len(limits) == 2:
             self.current, self.finish = limits[0], limits[1]
+        elif len(limits) == 3:
+            self.current, self.finish, self.step = limits[0], limits[1], limits[2]
         else:
             self.finish = limits[0]
             self.current = 1
@@ -78,13 +90,14 @@ class RangeFunction():
         if curr > self.finish:
             raise StopIteration
 
-        self.current += 1
+        self.current += self.step
 
         return curr
 
 
 range_with_2_param = RangeFunction(2, 6)
 range_with_1_param = RangeFunction(9)
+range_with_3_param = RangeFunction(0, 10, 2)
 
 for i in range_with_2_param:
     print(i)
@@ -92,6 +105,8 @@ for i in range_with_2_param:
 for i in range_with_1_param:
     print(i)
 
+for i in range_with_3_param:
+    print(i)
 
 # Task 3
 class Str(str):
